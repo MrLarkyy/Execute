a409fbdc42815590b0b87c6bba990428caa3d1af
MrLarkyy_ <studiotadikl@gmail.com>
2025-10-09 21:48:42 +0200

    Add extension functions for handling `ConditionHandle` and `ActionHandle` collections

---
+++ src/main/kotlin/gg/aquatic/execute/CollectionExt.kt
@@ -0,0 +1,34 @@
+package gg.aquatic.execute
+
+import gg.aquatic.execute.action.ActionHandle
+import gg.aquatic.execute.requirement.ConditionHandle
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+
+fun <T> Collection<ConditionHandle<T>>.checkRequirements(
+    binder: T,
+    textUpdater: (T, String) -> String = { _, str -> str }
+): Boolean {
+    for (configuredRequirement in this) {
+        if (!configuredRequirement.execute(binder) { _, str -> str }) return false
+    }
+    return true
+}
+
+fun <T : Any> Collection<ActionHandle<T>>.executeActions(
+    binder: T,
+    textUpdater: (T, String) -> String = { _, str -> str }
+) {
+    for (configuredAction in this) {
+        configuredAction.execute(binder, textUpdater)
+    }
+}
+
+fun Collection<ActionHandle<Player>>.broadcastActions(textUpdater: (Player, String) -> String = { _, str -> str }) {
+    for (player in Bukkit.getOnlinePlayers()) {
+        for (configuredAction in this) {
+            configuredAction.execute(player, textUpdater)
+        }
+    }
+
+}
