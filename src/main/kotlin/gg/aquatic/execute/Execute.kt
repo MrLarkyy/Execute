d3bf61368b39b843624b1f621e03b079511c1830
MrLarkyy_ <studiotadikl@gmail.com>
2026-03-26 00:38:38 +0100

    Add new condition implementations to Execute.kt
    Introduced multiple condition classes such as BiomeCondition, DateRangeCondition, and TimeRangeCondition. Updated the injectConditions function to register these new conditions for expanded functionality.

--- src/main/kotlin/gg/aquatic/execute/Execute.kt
+++ src/main/kotlin/gg/aquatic/execute/Execute.kt
@@ -6,7 +6,18 @@ import gg.aquatic.common.initializeCommon
 import gg.aquatic.execute.action.impl.*
 import gg.aquatic.execute.action.impl.logical.ConditionalActionsAction
 import gg.aquatic.execute.action.impl.logical.SmartAction
+import gg.aquatic.execute.condition.impl.DateRangeCondition
+import gg.aquatic.execute.condition.impl.DayOfMonthCondition
+import gg.aquatic.execute.condition.impl.DayOfWeekCondition
+import gg.aquatic.execute.condition.impl.BiomeCondition
+import gg.aquatic.execute.condition.impl.HasEmptyInventorySlotCondition
+import gg.aquatic.execute.condition.impl.MonthCondition
+import gg.aquatic.execute.condition.impl.OnlinePlayerCountCondition
 import gg.aquatic.execute.condition.impl.PermissionCondition
+import gg.aquatic.execute.condition.impl.TimeRangeCondition
+import gg.aquatic.execute.condition.impl.WeekOfYearModuloCondition
+import gg.aquatic.execute.condition.impl.WeekParityCondition
+import gg.aquatic.execute.condition.impl.WorldCondition
 import gg.aquatic.kregistry.bootstrap.BootstrapHolder
 import gg.aquatic.kregistry.bootstrap.ContributionBuilder
 import org.bukkit.plugin.java.JavaPlugin
@@ -44,7 +55,18 @@ object Execute {
 
     fun injectConditions(builder: ContributionBuilder) {
         builder.registry(Condition.REGISTRY_KEY) {
+            add("biome", BiomeCondition)
+            add("date-range", DateRangeCondition)
+            add("day-of-month", DayOfMonthCondition)
+            add("day-of-week", DayOfWeekCondition)
+            add("has-empty-inventory-slot", HasEmptyInventorySlotCondition)
+            add("month", MonthCondition)
+            add("online-player-count", OnlinePlayerCountCondition)
             add("permission", PermissionCondition)
+            add("time-range", TimeRangeCondition)
+            add("week-parity", WeekParityCondition)
+            add("week-of-year-mod", WeekOfYearModuloCondition)
+            add("world", WorldCondition)
         }
     }
 }
