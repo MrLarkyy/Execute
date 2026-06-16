1f57e1cdc8e5b802c98a81806fa59bea102a0d33
MrLarkyy_ <studiotadikl@gmail.com>
2026-06-17 00:57:23 +0200

    Bump Kotlin to 2.4.0, update dependencies, and increment Common to 26.0.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.3.20"
+    kotlin("jvm") version "2.4.0"
     id("co.uzzu.dotenv.gradle") version "4.0.0"
     `maven-publish`
     `java-library`
@@ -19,13 +19,13 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
 
     api("gg.aquatic:KRegistry:25.0.3")
     api("gg.aquatic:KEvent:26.0.5")
     api("gg.aquatic:Dispatch:26.0.5")
     api("gg.aquatic:Dispatch-bukkit:26.0.5")
-    api("gg.aquatic:Common:26.0.16")
+    api("gg.aquatic:Common:26.0.17")
 }
 
 kotlin {
